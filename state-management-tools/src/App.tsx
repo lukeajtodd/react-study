bb0ca322f66a3ec024b01069a3cdf4dba6e19c3a
Luke Todd <luketodd@zoho.com>
2022-01-11 22:17:24 +0000

    use reducer section added

--- state-management-tools/src/App.tsx
+++ state-management-tools/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import UseState from './pages/UseState'
+import UseReducer from './pages/UseReducer'
 
 const App = () => {
   return (
@@ -14,11 +15,15 @@ const App = () => {
                 <li className="text-md text-white tracking-wide cursor-pointer hover:border-white hover:border-b-2 border-b-2 border-transparent transition duration-300">
                   <Link to="/">useState</Link>
                 </li>
+                <li className="text-md text-white tracking-wide cursor-pointer hover:border-white hover:border-b-2 border-b-2 border-transparent transition duration-300">
+                  <Link to="/use-reducer">useReducer</Link>
+                </li>
               </ul>
             </div>
           </nav>
           <Routes>
             <Route path="/" element={<UseState />} />
+            <Route path="/use-reducer" element={<UseReducer />} />
           </Routes>
         </div>
       </Router>
---
+++ state-management-tools/src/pages/UseReducer.tsx
@@ -0,0 +1,80 @@
+import React, { useState, useReducer, Reducer } from 'react'
+
+type State = string[]
+enum ActionKind {
+  Add = 'ADD',
+  Pop = 'POP',
+}
+type Action = {
+  type: ActionKind
+  payload: string
+}
+
+const toolReducer: Reducer<State, Action> = (tools: string[], action) => {
+  const { payload: tool, type } = action
+
+  switch (type) {
+    case ActionKind.Add:
+      if (!tool) return tools
+      
+      return [...tools, tool]
+    case ActionKind.Pop:
+      if (tools.length == 0) return []
+
+      const clone = [...tools]
+      clone.pop()
+      return clone
+    default:
+      return tools
+  }
+}
+
+const UseReducer = () => {
+  const [newTool, setNewTool] = useState('')
+  const [tools, interactTools] = useReducer(toolReducer, [
+    'useState',
+    'useReducer',
+    'MobX',
+  ])
+
+  const addTool = () => {
+    interactTools({ payload: newTool, type: ActionKind.Add })
+    setNewTool('')
+  }
+
+  const popTool = () => {
+    interactTools({ payload: '', type: ActionKind.Pop })
+  }
+
+  return (
+    <div
+      style={{ height: '600px' }}
+      className="flex justify-center items-center"
+    >
+      <div>
+        <h1 className="text-2xl mb-4">useReducer</h1>
+        <ul>
+          {tools.map((tool) => (
+            <li>{tool}</li>
+          ))}
+        </ul>
+        <button className="mt-4" onClick={popTool}>
+          Pop
+        </button>
+        <div className="mt-2">
+          <input
+            type="text"
+            placeholder="Tool"
+            value={newTool}
+            onChange={(e) => setNewTool(e.target.value)}
+          />
+          <button className="mt-4" onClick={addTool}>
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UseReducer
